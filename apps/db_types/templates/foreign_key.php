2e854eb2c408ccf5bd87fa522de66ea2de9d7b8e
root <root@phutop.(none)>
2010-04-02 22:56:05 -0500

    Minor edit for temporary form generation use of id instead of spec'ed name

--- apps/db_types/templates/foreign_key.php
+++ apps/db_types/templates/foreign_key.php
@@ -3,6 +3,6 @@
 <br />
 <select name="<?php print Response::$context['field_name']; ?>" style="width:400px;">
     <?php foreach (Response::$context['field_options'] as $obj): ?>
-        <option value="<?php print $obj->id; ?>"<?php if ($obj->id == Response::$context['field_value']): ?> selected="selected"<?php endif; ?>><?php print $obj->first_name; ?></option>
+        <option value="<?php print $obj->id; ?>"<?php if ($obj->id == Response::$context['field_value']): ?> selected="selected"<?php endif; ?>><?php print $obj->id; ?></option>
     <?php endforeach; ?>
 </select>
