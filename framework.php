10adfd9cf302c50711006f66685083b6a2310906
phu <phu@kungphu.com>
2010-04-02 19:01:01 -0500

    Adding most superglobals to the Request class

--- framework.php
+++ framework.php
@@ -131,7 +131,14 @@ foreach ($declared as $classname)
 }
 
 
-Request::$params = $_REQUEST;
+Request::$params = &$_REQUEST;
+Request::$session = &$_SESSION;
+Request::$get = &$_GET;
+Request::$post = &$_POST;
+Request::$files = &$_FILES;
+Request::$cookie = &$_COOKIE;
+Request::$server = &$_SERVER;
+Request::$env = &$_ENV;
 
 Controller::process($_SERVER['QUERY_STRING']);
 
